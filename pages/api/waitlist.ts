980bc1746eb5ca4d39c6029abb91c69f04820091
ycpark <hipyohip@naver.com>
2025-05-28 00:03:27 +0900

    add runtime edge code

--- pages/api/waitlist.ts
+++ pages/api/waitlist.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export const runtime = 'edge';
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).end()
 
