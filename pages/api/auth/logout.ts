c8e75ad1d334f528a432869c9b39e5c30e5eaa73
ycpark <hipyohip@naver.com>
2025-06-06 00:40:01 +0900

    NO-ISSUE change moduleResolution

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,22 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import cookie from 'cookie';
+import * as cookie from 'cookie';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method not allowed' });
+    }
 
-  // Clear the authentication cookie
-  res.setHeader(
-    'Set-Cookie',
-    cookie.serialize('accessToken', '', {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      expires: new Date(0), // Set expiration to the past to delete the cookie
-      path: '/',
-      sameSite: 'strict',
-    })
-  );
+    // 인증 쿠키 제거
+    res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('accessToken', '', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            expires: new Date(0), // 과거 날짜로 삭제
+            path: '/',
+            sameSite: 'strict',
+        })
+    );
 
-  return res.status(200).json({ success: true });
+    return res.status(200).json({ success: true });
 }
